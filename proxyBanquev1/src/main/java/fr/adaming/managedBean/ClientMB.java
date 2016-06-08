21cfd0dfa646c1ac5ae91b4389d1351dadce7856
INTI-0247 <INTI-0247@192.168.100.154>
2016-06-08 17:29:00 +0200

    ClientMB le bon cette fois

--- proxyBanquev1/src/main/java/fr/adaming/managedBean/ClientMB.java
+++ proxyBanquev1/src/main/java/fr/adaming/managedBean/ClientMB.java
@@ -1,9 +1,37 @@
 package fr.adaming.managedBean;
+import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import fr.adaming.dao.ClientDAO;
+import fr.adaming.model.Client;
+import fr.adaming.service.ClientService;
 
 @ManagedBean
-public class ClientMB {
+public class ClientMB implements Serializable{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Autowired
+	private ClientService clientService;
+	
+	public ClientMB() {
+		clientService = new ClientService();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public void addClient(Client client){
+		clientService.add(client);
+	}
+	
+	
+	
+	
+	
 }
